7a49bff834b77d7ae612a905ea74cb637bcab38c
sonjarvis <sonjarvis96@gmail.com>
2025-02-26 20:52:47 +0900

    [Bronze III] Title: 최소, 최대, Time: 316 ms, Memory: 129196 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,13 +1,4 @@
-number = int(input())
-number_list = list(map(int, input().split()))
+N = int(input())
+arr = list(map(int, input().split()))
 
-max_num = number_list[0]
-min_num = number_list[0]
-for num in number_list:
-    
-    if num > max_num:
-        max_num = num
-    if num < min_num:
-        min_num = num
-
-print(min_num, max_num)
+print(min(arr), max(arr))
