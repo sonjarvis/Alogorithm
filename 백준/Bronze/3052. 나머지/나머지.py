56d0084b454988d1b1ca1132e75779a99c50fc6f
sonjarvis <sonjarvis96@gmail.com>
2025-03-02 11:08:05 +0900

    [Bronze II] Title: 나머지, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,14 +1,7 @@
-num_list = []
-rem_list = []
+arr = []
 
 for i in range(10):
-    num_list.append(int(input()))
-
-for num in num_list:
-    rem = num % 42
-    if rem in rem_list:
-        pass
-    else:
-        rem_list.append(rem)
-        
-print(len(rem_list))
+    num = int(input())
+    if num % 42 not in arr:
+        arr.append(num % 42)
+print(len(arr))
