5e80af3a85083f504e6ee77ca95f3d92b60009aa
sonjarvis <sonjarvis96@gmail.com>
2025-03-05 23:44:38 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 56 ms, Memory: 29284 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,20 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+word_list = []
+word_num = []
+
+word = str(input())
+
+
+for i in word:
+    word_list.append(i)
+
+for character in alphabet:
+    for i in range(len(word_list)):
+        if character == word_list[i]:
+            word_num.append(i)
+            break
+        elif i < len(word_list)-1: continue
+        else:
+            word_num.append(-1)
+for i in word_num:
+    print(i,end=" ")
