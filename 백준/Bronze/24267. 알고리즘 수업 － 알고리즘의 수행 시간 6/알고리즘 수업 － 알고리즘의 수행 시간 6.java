c7a9785c91308484c0574838f57435a10e2bb9eb
sonjarvis <sonjarvis96@gmail.com>
2025-04-06 12:38:47 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 176 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        sc.close();
+
+        System.out.println((n * (n - 1) * (n - 2)) / 6);
+        System.out.println(3);
+    }
+}
