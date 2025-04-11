e220ca65bd6843f9e3ad3113c2839487b5cc150f
sonjarvis <sonjarvis96@gmail.com>
2025-04-12 01:07:26 +0900

    [Silver IV] Title: 설탕 배달, Time: 56 ms, Memory: 29288 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,20 @@
+order = int(input())
+
+if order % 5 == 0:
+    print(order // 5)
+
+elif order % 5 == 3:
+    print(order // 5 + 1)
+
+elif order // 5 - 1 >= 0 and order - (5 * (order // 5 - 1)) == 6:
+    print((order // 5 - 1) + 2)
+
+elif order // 5 - 1 >= 0 and order - (5 * (order // 5 - 1)) == 9:
+    print((order // 5 - 1) + 3)
+
+elif order // 5 - 2 >= 0 and order - (5 * (order // 5 - 2)) == 12:
+    print((order // 5 - 2) + 4)
+
+else:
+    print(-1)
+
