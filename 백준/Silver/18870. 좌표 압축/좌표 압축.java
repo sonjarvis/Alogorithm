f74fc3bf3e6dc562da87b0b73bdc62f3034106cd
sonjarvis <sonjarvis96@gmail.com>
2025-04-20 20:03:00 +0900

    [Silver II] Title: 좌표 압축, Time: 2720 ms, Memory: 329864 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,42 @@
+import java.util.*;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		
+		int[] origin = new int[N];
+		int[] sorted = new int[N];
+		HashMap<Integer, Integer> rankingMap = new HashMap<Integer, Integer>();
+ 
+		
+		for(int i = 0; i < N; i++) {
+			sorted[i] = origin[i] = in.nextInt();
+		}
+		
+		Arrays.sort(sorted);
+		
+		
+		int rank = 0;
+		for(int v : sorted) {
+			if(!rankingMap.containsKey(v)) {
+				rankingMap.put(v, rank);
+				rank++;		
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int key : origin) {
+			int ranking = rankingMap.get(key);
+			sb.append(ranking).append(' ');
+		}
+		
+		System.out.println(sb);
+		
+		
+ 
+	}
+}
